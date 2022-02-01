15fa9d5a47f24e4c48789a0be568ef4985c6ad26
Favour Wilson Ebrusike <41411068+FavourWilson@users.noreply.github.com>
2022-02-01 02:49:48 +0100

    change url

--- config/app.php
+++ config/app.php
@@ -51,8 +51,7 @@ return [
     | your application so that it is used when running Artisan tasks.
     |
     */
-
-    'url' => env('APP_URL', 'http://localhost'),
+https://ecommerce-freecodingboy.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
