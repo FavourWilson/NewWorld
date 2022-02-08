c929df05b08e9f8801cfd138b45f99792fa297e8
Favour Wilson Ebrusike <41411068+FavourWilson@users.noreply.github.com>
2022-02-08 22:19:43 +0100

    Update logo.blade.php

--- resources/views/layout/NewWorld/navs/logo.blade.php
+++ resources/views/layout/NewWorld/navs/logo.blade.php
@@ -159,7 +159,7 @@
                                        
                                         <span class="d-none d-xl-block font-weight-bold font-size-16 text-gray-90 ml-3"></span>
                                     </a>
-                                    else
+                                    @else
                                      <a href="{{url('NewWorld/cart/'.$cart[0]->id)}}" class="text-gray-90 position-relative d-flex " data-toggle="tooltip" data-placement="top" title="Cart">
                                         <i class="font-size-22 ec ec-shopping-bag"></i>
                                        
